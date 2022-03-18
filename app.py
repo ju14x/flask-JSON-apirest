df14fefff9c4f04b26571ec221dd4ade1e4e82ff
Juliax <jfcaiado@hotmail.com>
2022-03-18 20:07:48 -0300

    Add routes to views.py for better organization

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+from views import *
 
 if __name__ == '__main__':
     app.run(debug=True)
