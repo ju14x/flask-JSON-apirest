dbedcb6b178dda6302e0c73bc22b0bae9d3687da
Juliax <jfcaiado@hotmail.com>
2022-03-23 12:30:27 -0300

    Redo data treatment/processing

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
+from datetime import datetime
 import httpx
-from flask import Flask, request, jsonify
-from flask_pydantic_spec import (
-    FlaskPydanticSpec, Response, Request
-)
+from flask import Flask, jsonify
+from flask_pydantic_spec import (FlaskPydanticSpec)
 
 app = Flask(__name__)
 spec = FlaskPydanticSpec('flaskinho', title='Flaskinho API REST')
 spec.register(app)  # registering endpoints
 
-url = 'https://jsonplaceholder.typicode.com/todos'
+url = 'https://jsonplaceholder.typicode.com/todos/'
 
 
 @app.get('/')
-def five_first_JSONs():
+def get_five():
     """Returns first five JSONs from external API"""
-    result = httpx.get(url)
-    todo_list = result.json()
-    todo_data = todo_list[0:5]
-    return jsonify(todo_data)
-        
+    r = httpx.get(url)
+    data = r.json()[0:5]
+    return jsonify(data, datetime.now(), r.status_code)
+
 
 if __name__ == '__main__':
     app.run()
