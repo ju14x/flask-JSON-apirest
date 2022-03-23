ec0e7aa0086077662b1f3760c0205d02335490f7
Juliax <jfcaiado@hotmail.com>
2022-03-23 13:34:33 -0300

    Add logging with timestamp and HTTP status code

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import logging
+
 import httpx
 from flask import Flask, jsonify
 from flask_pydantic_spec import (FlaskPydanticSpec)
@@ -9,13 +11,21 @@ spec.register(app)  # registering endpoints
 
 url = 'https://jsonplaceholder.typicode.com/todos/'
 
+logging.basicConfig(
+    filename='record.log',
+    encoding='utf-8', 
+    level=logging.DEBUG, 
+    format = f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S'
+)
+
 
 @app.get('/')
 def get_five():
     """Returns first five JSONs from external API"""
     r = httpx.get(url)
     data = r.json()[0:5]
-    return jsonify(data, datetime.now(), r.status_code)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
